bbe747f6afbbc93f359afed9caae60ac0ec9699a
Lucjan Zenczuk <lucjan.zenczuk@gmail.com>
2019-05-23 22:34:13 +0100

    Refactor of product editor page - in progress
    Change page widget to stateful
    Improved design (padding...)
    Add focus support - not fully implemented

--- lib/pages/products/product_editor.dart
+++ lib/pages/products/product_editor.dart
@@ -1,76 +1,120 @@
 import 'package:diet_app/models/product.dart';
 import 'package:flutter/material.dart';
 
-class ProductEditorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Product"),
-      ),
-      body: ProductForm(),
-    );
-  }
-}
+class ProductEditorPage extends StatefulWidget {
+  final Product product;
 
-class ProductForm extends StatefulWidget {
+  const ProductEditorPage({Key key, this.product}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _ProductFormState();
+    return _ProductEditorState(product);
   }
 }
 
-class _ProductFormState extends State<ProductForm> {
+class _ProductEditorState extends State<ProductEditorPage> {
   final _formKey = GlobalKey<FormState>();
 
-  Product _product = Product();
+  Product _product;
+
+  _ProductEditorState(Product product) {
+    if (product != null) {
+      _product = product;
+    } else {
+      _product = Product();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Product name'),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter product name';
-              }
-            },
-            onSaved: (value) => setState(() => _product.name = value),
+    var nameFocusNode = FocusNode();
+    var fatFocusNode = FocusNode();
+    var carbohydrateFocusNode = FocusNode();
+    var proteinFocusNode = FocusNode();
+
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          title: Text("Product"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("save"),
+              onPressed: () {
+                validateAndSave(context);
+              },
+            )
+          ],
+        ),
+        body: SafeArea(
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Product name'),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter product name';
+                    }
+                  },
+                  onSaved: (value) => setState(() => _product.name = value),
+                  textInputAction: TextInputAction.next,
+                  focusNode: nameFocusNode,
+                  onFieldSubmitted: (term) {
+                    nameFocusNode.unfocus();
+                    FocusScope.of(context).requestFocus(fatFocusNode);
+                  },
+                ),
+                Text('Nutritions'),
+                NumberFormField(
+                  fieldName: 'Fat',
+                  messageWhenEmpty: 'Please enter amount of fat',
+                  onSaved: (value) => setState(() => _product.fat = value),
+                  textInputAction: TextInputAction.next,
+                  focusNode: fatFocusNode,
+                  onFieldSubmitted: (term) {
+                    fatFocusNode.unfocus();
+                    FocusScope.of(context).requestFocus(carbohydrateFocusNode);
+                  },
+                ),
+                NumberFormField(
+                  fieldName: 'Carbohydrate',
+                  messageWhenEmpty: 'Please enter amount of carbohydrate',
+                  onSaved: (value) =>
+                      setState(() => _product.carbohydrates = value),
+                  textInputAction: TextInputAction.next,
+                  focusNode: carbohydrateFocusNode,
+                  onFieldSubmitted: (term) {
+                    carbohydrateFocusNode.unfocus();
+                    FocusScope.of(context).requestFocus(proteinFocusNode);
+                  },
+                ),
+                NumberFormField(
+                  fieldName: 'Protein',
+                  messageWhenEmpty: 'Please enter amount of protein',
+                  onSaved: (value) => setState(() => _product.protein = value),
+                  textInputAction: TextInputAction.done,
+                  focusNode: proteinFocusNode,
+                  onFieldSubmitted: (term) {
+                    validateAndSave(context);
+                  },
+                )
+              ],
+            ),
           ),
-          Text('Nutritions'),
-          NumberFormField(
-            fieldName: 'Fat',
-            messageWhenEmpty: 'Please enter amount of fat',
-            onSaved: (value) => setState(() => _product.fat = value),
-          ),
-          NumberFormField(
-            fieldName: 'Carbohydrate',
-            messageWhenEmpty: 'Please enter amount of carbohydrate',
-            onSaved: (value) => setState(() => _product.carbohydrates = value),
-          ),
-          NumberFormField(
-            fieldName: 'Protein',
-            messageWhenEmpty: 'Please enter amount of protein',
-            onSaved: (value) => setState(() => _product.protein = value),
-          ),
-          RaisedButton(
-            child: Text('Submit'),
-            onPressed: () {
-              if (_formKey.currentState.validate()) {
-                _formKey.currentState.save();
-                Navigator.pop(context, _product);
-              }
-            },
-          )
-        ],
-      ),
-    );
+        ));
+  }
+
+  void validateAndSave(BuildContext context) {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      Navigator.pop(context, _product);
+    }
   }
 }
 
@@ -78,33 +122,46 @@ class NumberFormField extends StatelessWidget {
   final String fieldName;
   final String messageWhenEmpty;
   final FormFieldSetter<double> onSaved;
+  final TextInputAction textInputAction;
+  final FocusNode focusNode;
+  final ValueChanged<String> onFieldSubmitted;
 
   const NumberFormField(
-      {Key key, this.fieldName, this.messageWhenEmpty, this.onSaved})
+      {Key key,
+      this.fieldName,
+      this.messageWhenEmpty,
+      this.onSaved,
+      this.textInputAction,
+      this.focusNode,
+      this.onFieldSubmitted})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-        decoration: InputDecoration(hintText: fieldName),
-        keyboardType: TextInputType.number,
-        validator: (value) {
-          if (value.isEmpty) {
-            return messageWhenEmpty;
-          }
+      decoration: InputDecoration(labelText: fieldName),
+      keyboardType: TextInputType.number,
+      textInputAction: this.textInputAction,
+      focusNode: this.focusNode,
+      validator: (value) {
+        if (value.isEmpty) {
+          return messageWhenEmpty;
+        }
 
-          var num = double.tryParse(value);
-          if (num == null) {
-            return 'Only numbers allowed';
-          }
+        var num = double.tryParse(value);
+        if (num == null) {
+          return 'Only numbers allowed';
+        }
 
-          if (num < 0) {
-            return 'Only numbers bigger then 0 allowed';
-          }
-        },
-        onSaved: (value) {
-          double num = double.parse(value);
-          onSaved(num);
-        });
+        if (num < 0) {
+          return 'Only numbers bigger then 0 allowed';
+        }
+      },
+      onSaved: (value) {
+        double num = double.parse(value);
+        onSaved(num);
+      },
+      onFieldSubmitted: (term) => onFieldSubmitted(term),
+    );
   }
 }
